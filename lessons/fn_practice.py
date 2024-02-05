e59d958623d30c94e03d52f58582599d4f338bac
Dillon Cavanagh <dcavanagh@unc.edu>
2024-02-05 13:25:15 -0500

    Fix

--- lessons/fn_practice.py
+++ lessons/fn_practice.py
@@ -3,8 +3,8 @@
 def my_max(num1: int, num2: int) -> int:
     """Returns the maximum value out of two numbers"""
     if num1 >= num2:
-        return num1 + 0
-    else: #number1 < number2
+        return num1 + 0   
+    else: #number1 < number2 
         return num2
 
 max: int = my_max(1,12)
