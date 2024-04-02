41eb33466fff8088d1fbc51a74f9acf4f5b71d92
Dillon Cavanagh <dcavanagh@unc.edu>
2024-04-02 09:14:22 -0400

    EX07

--- exercises/ex07/runtime_analysis_functions.py
+++ exercises/ex07/runtime_analysis_functions.py
@@ -1,17 +1,36 @@
 import numpy as np
 import timeit
 import tracemalloc
+from random import randint
 
 MAX_VAL: int = 10 ** 5
 
 def random_descending_list(n: int) -> list[int]:
     """Generate a list of random descending integers."""
-    new_list: list[int] = []
+    new_list: list[int] = [MAX_VAL]
+    idx: int = 0
+    while idx < n:
+        value: int = randint(-10 ** 5,10 ** 5)
+        new_list.append(value)
+        idx1: int = idx
+        idx2: int = idx1 + 1
+        tempidx: int = idx2
+        while idx1 >= 0:
+            if new_list[idx1] >= new_list[tempidx]:
+                idx1 -= 1
+            else:
+                temp: int = new_list[idx1]
+                new_list[idx1] = new_list[tempidx]
+                new_list[tempidx] = temp
+                tempidx = idx1
+                idx1 -= 1
+        idx += 1
+    new_list.pop(0)
     return new_list
 
 def evaluate_runtime(fn_name, start_size: int, end_size: int) -> np.array:
     """Evaluate the runtime for different size inputs."""
-    from exercises.ex07.sort_functions import selection_sort, insertion_sort
+    from sort_functions import selection_sort, insertion_sort
     NUM_TRIALS: int = 1
     times: list[float] = []
     for inp_size in range(start_size, end_size+1):
@@ -24,7 +43,7 @@ def evaluate_runtime(fn_name, start_size: int, end_size: int) -> np.array:
     return np.array(times)
 
 def evaluate_memory_usage(fn_name, start_size: int, end_size: int):
-    from exercises.ex07.sort_functions import selection_sort, insertion_sort
+    from sort_functions import selection_sort, insertion_sort
     usage: list[float] = []
     for inp_size in range(start_size, end_size+1):
         l: list[int] = random_descending_list(inp_size)
--- exercises/ex07/sort_functions.py
+++ exercises/ex07/sort_functions.py
@@ -1,15 +1,47 @@
 """Functions that implement sorting algorithms."""
 
-__author__ = ""
+__author__ = "730499558"
 
 def insertion_sort(x: list[int]) -> None:
     """Basic insertion sort algorithm.
     Insert into an already sorted list."""
+    idx1: int = 0
+    while idx1 < len(x)-1:
+        idx2: int = idx1 + 1
+        tempidx: int = idx2
+        while idx1 >= 0:
+            if x[idx1] <= x[tempidx]:
+                idx1 -= 1
+            else:
+                temp: int = x[idx1]
+                x[idx1] = x[tempidx]
+                x[tempidx] = temp
+                tempidx = idx1
+                idx1 -= 1
+        idx1 = idx2
     return None
 
 
 def selection_sort(x: list[int]) -> None:
     """Basic selection sort algorithm. 
     Repeatedly find the minimum and swap it."""
+    idx1: int = 0
+    while idx1 < len(x):
+        idx2: int = idx1 + 1
+        minidx: int = idx2
+        if idx2 < len(x):
+            min: int = x[idx2]
+            minidx: int = idx2
+        while idx2 < len(x):
+            if min > x[idx2]:
+                min = x[idx2]
+                minidx = idx2
+            idx2 += 1
+        if minidx < len(x):
+            if x[minidx] < x[idx1]:
+                temp: int = x[idx1]
+                x[idx1] = x[minidx]
+                x[minidx] = temp
+        idx1 += 1
     return None
     
